af46ce33bfe4935d69f31396d1bd38174585df23
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2019-09-19 19:34:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 def display_board(array)
   puts " #{array[0]} | #{array[1]} | #{array[2]} "
   puts "-----------"
-  puts " " " " " " "|" " " " " " " "|"
+  puts "   |   |   "
   puts "-----------"
-  puts " " " " " " "|" " " " " " " "|"
+  puts "   |   |   "
 end
