023a65c1a62c772122daa234d4813480af512a70
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2019-09-19 19:41:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -23,3 +23,27 @@ def display_board(array)
   puts "-----------"
   puts "   |   |   "
 end
+
+def display_board(array)
+  puts " #{array[0]}  |   |   "
+  puts "-----------"
+  puts "   | #{array[4] } |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def display_board(array)
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def display_board(array)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
+end
